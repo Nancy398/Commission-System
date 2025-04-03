4d0cd7b9db135ac8a44e97643e1105421f8e80fb
Nancy398 <nancy@moohousing.com>
2025-04-02 18:53:00 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ st.title("🔑 User Login")
 # ---- 处理激活链接 ----
 activation_params = st.query_params
 # activation_code = activation_params.get("activate", [None])[0]
-activation_code = activation_params.get("activate"）
+activation_code = activation_params.get("activate")
 st.write(activation_code)
 st.write(activation_params)
 # 如果 URL 里有 activation_code，则显示激活界面
